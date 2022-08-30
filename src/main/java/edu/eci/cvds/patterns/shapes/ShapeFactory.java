0b7f23ce4895aa7306fc214c7ba54dadecd43a3e
carol695 <63822072+carol695@users.noreply.github.com>
2022-08-29 19:06:14 -0500

    Update ShapeFactory.java

--- src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
+++ src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
@@ -1,4 +1,21 @@
 package edu.eci.cvds.patterns.shapes;
 
 public class ShapeFactory {
+  public static Shape create(regularShapeType type){
+    switch(type){
+      case Triangle:
+            return new Triangle();
+        
+      case Quadrilateral:
+          return new Quadrilateral();
+        
+      case Pentagon:
+          return new Pentagon();
+        
+      case Hexagon: 
+          return new Hexagon();
+        
+      default:
+          return null;
+    }
 }
